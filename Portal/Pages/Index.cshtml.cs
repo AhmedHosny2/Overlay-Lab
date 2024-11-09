1bb73448c1a2a833ee7b35fbea08b813e74476ad
Ahmed Yehia <98207790+AhmedHosny2@users.noreply.github.com>
2024-11-09 13:42:51 +0100

    pull image if not there and deploy container

--- Portal/Pages/Index.cshtml.cs
+++ Portal/Pages/Index.cshtml.cs
@@ -9,80 +9,152 @@ namespace Portal.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
-    public string? Name { get; set; }
     // list od containers data
     public IList<ServerInstance> Containers { get; set; } = new List<ServerInstance>();
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
     }
-    public async void OnPost()
-    {
-        Console.WriteLine("Name: " + Request.Form["Name"]);
-        Name = Request.Form["Name"];
-      
-
-    }
-
-public async Task OnGetAsync()    {
-          DockerClient client =  new  DockerClientConfiguration(
-    new Uri("unix:///var/run/docker.sock")) // todo what is this ? 
-     .CreateClient();
-
-
-
-    // Optionally pull the image or log an error message
-    // await client.Images.CreateImageAsync(
-    //     new ImagesCreateParameters { FromImage = "ubuntu", Tag = "latest" },
-    //     null,
-    //     new Progress<JSONMessage>());
-
-
-
-
-    // await client.Containers.CreateContainerAsync(new CreateContainerParameters()
-    // {
-    //     Image = "hello-world",
-    //     HostConfig = new HostConfig()
-    //     {
-    //         DNS = new[] { "8.8.8.8", "8.8.4.4" }
-    //     }
-    // });
-
-
-
-
-
-
-
-    // list containers
-
-        IList<ContainerListResponse> containers = await client.Containers.ListContainersAsync(
-            new ContainersListParameters()
-            {
-                Limit = 10,
-            });
-        foreach (var container in containers)
-        {
-          
-            Containers.Add(
-                new ServerInstance
-                {
-                    InstanceId = container.ID.Substring(0, 5),
-                    ServerType = container.Image.Substring(0, 30),
-                    Status = container.Status   ,
-                    IpAddress = "X.X.X.X", // todo double chick 
-                    Port = "X.X.X.X",
-                    Created = container.Created
-                }
-            );
-
-
-
-        }
+    public async Task OnPostAsync()    {
+        await MainAsync();
        
 
 
+    }
+    public async Task MainAsync()
+    {
+        // create a new DockerClient object 
+        DockerClient client = new DockerClientConfiguration(
+      new Uri("unix:///var/run/docker.sock")) // todo what is this ? 
+       .CreateClient();
+        // get user's images and check if he got hello-world image
+        IList<ImagesListResponse> images = await client.Images.ListImagesAsync(new ImagesListParameters()
+        {
+            // get hello-world image
+            Filters = new Dictionary<string, IDictionary<string, bool>>()
+            {
+                {
+                    "reference", new Dictionary<string, bool>()
+                    {
+                        { "hello-world", true }
+                    }
+                }
+            }
+        });
+        // check if images is not null and print the details
+        if (images.Count == 0)
+        {
+            // pull it first 
+            Console.WriteLine("Pulling hello-world image");
+            try
+            {
+                await client.Images.CreateImageAsync(
+            new ImagesCreateParameters
+            {
+                FromImage = "hello-world",
+                Tag = "latest",
+            },
+            null, // TODO: add your auth details 
+            new Progress<JSONMessage>());
+                Console.WriteLine("Image pulled successfully");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+        }
+
+        else
+        {
+
+            Console.WriteLine("Image found: ");
+            // Create docker container hello-world
+            Console.WriteLine("Creating container hello-world");
+            try
+            {
+                await client.Containers.CreateContainerAsync(new CreateContainerParameters()
+                {
+                    Image = "hello-world",
+                    HostConfig = new HostConfig()
+                    {
+                        DNS = new[] { "8.8.8.8", "8.8.4.4" }
+                    }
+                });
+                Console.WriteLine("Container created successfully");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+
+        // get user's images and print their details
+        // =================================================================================================
+
+
+        // foreach (var image in images)
+        // {
+        //     Console.WriteLine("Image ID: " + image.ID);
+
+        //     // Check if RepoTags is available and print each tag
+        //     if (image.RepoTags != null && image.RepoTags.Count > 0)
+        //     {
+        //         foreach (var tag in image.RepoTags)
+        //         {
+        //             Console.WriteLine("Image Name and Tag: " + tag);
+        //         }
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine("Image Name and Tag: <No Tags>");
+        //     }
+
+        //     Console.WriteLine("Created: " + image.Created);
+        //     Console.WriteLine("Size: " + image.Size);
+        // }
+        // =================================================================================================
+
+        // list containers
+        Console.WriteLine("Listing containers");
+        try
+        {
+            IList<ContainerListResponse> containers = await client.Containers.ListContainersAsync(
+                new ContainersListParameters()
+                {
+                    Limit = 10,
+                });
+            foreach (var container in containers)
+            {
+
+                Containers.Add(
+                    new ServerInstance
+                    {
+                        InstanceId = container.ID.Substring(0, 5),
+                        
+                        ServerType = container.Image.Split("@sha256")[0],
+                        Status = container.Status,
+                        IpAddress = "X.X.X.X", // todo double chick 
+                        Port = "X.X.X.X",
+                        Created = container.Created
+                    }
+                );
+
+
+
+            }
+            Console.WriteLine("Containers listed successfully");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+
+    }
+
+    // call the mainAsync method
+    public async Task OnGetAsync()
+    {
     }
 
 
