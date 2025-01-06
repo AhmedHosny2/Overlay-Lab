b7fcae61e34c823848043ede0be5a17587eb4f82
Ahmed Yehia <98207790+AhmedHosny2@users.noreply.github.com>
2025-01-06 15:12:05 +0100

    Update IDploymentService.cs

--- Portal/DeploymentService/Interface/IDploymentService.cs
+++ Portal/DeploymentService/Interface/IDploymentService.cs
@@ -13,7 +13,7 @@ namespace Portal.DeploymentService.Interface
     {
         public DockerClient CreateDockerClient();
         public Task EnsureDockerImageExists(DockerClient client, string ImageName);
-        public Task<string> GetOrCreateContainerForUser(DockerClient client, string ImageName, string exerciseName, string Uid, string port, string ip, bool isCLient, string clientPort,int MaxUsers);
+        public Task<string> GetOrCreateContainerForUser(DockerClient client, string ImageName, string exerciseName, string Uid, string port, string ip, bool isCLient, string clientPort, int MaxUsers);
         public Task<string> InitializeContainer(DockerClient client, string ImageName, string exerciseName, string Uid, string port, string ip, bool isCLient, string clientPort);
         public Task<IList<string>> ListUsersContainer(DockerClient client, string Uid);
         public Task<ServerInstance> FetchContainerDetails(DockerClient client, string exerciseName, List<string> DisplayFields, string Uid, string ip);
@@ -23,6 +23,6 @@ namespace Portal.DeploymentService.Interface
         public Task PauseContainer(DockerClient client, string ContainerId);
         // todo delete container 
         public Task RemoveUserOrPauseContainer(DockerClient client, string ContainerId, string Uid);
-        public Task ClientExercisePassed(uid, container_id);
+        public Task<string> ClientExercisePassed(string uid, string container_id);
     }
 }
